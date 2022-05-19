77f453b8d93ceba73b7950b8236328317fd67861
Richardaeld <Richardaeld@gmail.com>
2022-05-19 17:26:05 -0400

    cleaned up header

--- index.php
+++ index.php
@@ -9,40 +9,31 @@
     <link rel="stylesheet" href="index.css">
 </head>
 <body>
-    <h1>PHP Powered DM Dice Roll Tool</h1>
-    <h2>Pick One Of Our Die or Input Your Own!</h2>
-    <h3>
-        <div class="col-11 mt-5 mx-2 py-4 index-background flex-content-center" style='color:black'>
 
-            <?php
-                // inital api call
-                $api_dice = 'https://dnd-rolling-chart-api.herokuapp.com/api/button/main/viewAll/children/3';
-                $json_api_dice = file_get_contents($api_dice);
-                $dice_data = json_decode($json_api_dice);
-                // $dice_data -> button[2]  = new stdClass;
-                // $dice_data -> button[2] -> name  = "userInput";
-                // $dice_data -> button[2] -> obj_name  = "User input";
-                // $dice_data -> button[2] -> parent_foreign_key  = false;
-                // $dice_data -> button[2] -> radio_button  = [];
+    <header class="container-fluid">
+        <div class="row justify-content-center">
+            <h1 class="mt-4">PHP Powered DM Dice Roll Tool</h1>
+            <h2>Pick One Of Our Die or Input Your Own!</h2>
+            <div class="col-12 mt-5 mx-2 py-4 index-background flex-content-center" style='color:black'>
+                <?php
+                    // inital api call
+                    $api_dice = 'https://dnd-rolling-chart-api.herokuapp.com/api/button/main/viewAll/children/3';
+                    $json_api_dice = file_get_contents($api_dice);
+                    $dice_data = json_decode($json_api_dice);
 
-                foreach($dice_data -> button as $key => $value) {
-
-                    // $user_input_radio = $dice_data -> button[2] -> radio_button;
-                    // $user_input_radio = array(1, 2);
-                    // $dice_data -> button[2] -> radio_button = array($dice_data -> button[2] -> radio_button, $value -> name);
-
-                    echo '<form action="get" class="nonselectable hand-drawn-text hand-drawn-container-outer hand-drawn-border ">';
-                        $form_value = $value -> name;
-
-                        echo "<input type='hidden' name='${form_value}' placeholder='${form_value}' value='${form_value}'>";
-                        echo "<button class='hand-drawn-container-inner no-button' type='submit' name='level${form_value}' value='submit'>";
-                            echo $value -> obj_name;
-                        echo '</button>';
-                    echo '</form>';
-                }
-            ?>
+                    foreach($dice_data -> button as $key => $value) {
+                        echo '<form action="get" class="nonselectable hand-drawn-text hand-drawn-container-outer hand-drawn-border ">';
+                            $form_value = $value -> name;
+                            echo "<input type='hidden' name='${form_value}' placeholder='${form_value}' value='${form_value}'>";
+                            echo "<button class='hand-drawn-container-inner no-button' type='submit' name='level${form_value}' value='submit'>";
+                                echo $value -> obj_name;
+                            echo '</button>';
+                        echo '</form>';
+                    }
+                ?>
+            </div>
         </div>
-    </h3>
+    </header>
 
     <h4>
         <div class="col-11 mt-5 mx-2 py-4 flex-content-center" style="color:black">
@@ -56,10 +47,6 @@
 
                     if (isset($_GET[$value -> name])){
 
-                        // print_r($value -> parent_foreign_key);
-                        // print_r($value -> id);
-
-                        if ($value -> name !== "userInput") {
                             $parent_key =  $value -> id;
                             $api_roll_content = "https://dnd-rolling-chart-api.herokuapp.com/api/button/sub/viewAll/children/${parent_key}";
                             $json_api_roll_content = file_get_contents($api_roll_content);
@@ -67,9 +54,7 @@
                             $random_roll_num = mt_rand(0, count($roll_content_data -> button) - 1);
                             $random_roll_value = $roll_content_data -> button[$random_roll_num] -> value;
                             $random_roll_display_num = $random_roll_num + 1;
-                        }
 
-                        // print_r ($random_roll_value);
 
                         echo "<div class='container-fluid'>";
                             echo "<div class='row justify-content-center'>";
