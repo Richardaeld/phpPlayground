5669fbf0a35a3dd6d9b8b8818a1bb256e1891949
Richardaeld <Richardaeld@gmail.com>
2022-05-18 20:24:44 -0400

    Updated for dynamic input

--- index.php
+++ index.php
@@ -20,20 +20,15 @@
                 $json_api_dice = file_get_contents($api_dice);
                 $dice_data = json_decode($json_api_dice);
                 $dice_data -> button[2]  = new stdClass;
-                $dice_data -> button[2] -> name  = "User input";
+                $dice_data -> button[2] -> name  = "userInput";
                 $dice_data -> button[2] -> obj_name  = "User input";
                 $dice_data -> button[2] -> parent_foreign_key  = false;
-
-                // echo '<pre>';
-                //     print_r($dice_data -> button);
-                // echo '</pre>';
 
                 foreach($dice_data -> button as $key => $value) {
                     echo '<form action="get" class="nonselectable hand-drawn-text hand-drawn-container-outer hand-drawn-border ">';
                         $form_value = $value -> name;
 
-                        // echo $form_value = $form_value[-1];
-                        echo "<input type='hidden' name='lvl${form_value}' placeholder='${form_value}' value='${form_value}'>";
+                        echo "<input type='hidden' name='${form_value}' placeholder='${form_value}' value='${form_value}'>";
                         echo "<button class='hand-drawn-container-inner no-button' type='submit' name='level${form_value}' value='submit'>";
                             echo $value -> obj_name;
                         echo '</button>';
@@ -53,36 +48,42 @@
     </h3>
 
     <h4>
-        <div class="col-11 mt-5 mx-2 py-4 index-background flex-content-center" style="color:black">
+        <div class="col-11 mt-5 mx-2 py-4 flex-content-center" style="color:black">
             <?php
-                // foreach($levels_data -> button as $key => $value) {
-                //     if (isset($_GET["level${key}"])){
-                //         $api_spells = "https://dnd-rolling-chart-api.herokuapp.com/api/spells/spellsByLevel/${key}";
-                //         $json_api_spells = file_get_contents($api_spells);
-                //         $spells_data = json_decode($json_api_spells);
 
-                //         foreach($spells_data -> spells as $spell_key => $spell_value) {
-                //             $spell_name = $spell_value -> name;
-                //             $spell_id = $spell_value -> id;
-                //             echo '<form action="get" class="nonselectable hand-drawn-text hand-drawn-container-outer hand-drawn-border ">';
-                //                 echo "<input type='hidden' name='spell${spell_id}' placeholder='${spell_id}' value='${spell_id}'>";
+                echo '<pre>';
+                    print_r($dice_data -> button[1] -> id);
+                echo '</pre>';
 
-                //                 echo "<button class='hand-drawn-container-inner no-button' type='submit' name='spell${spell_id}' value='submit'>";
-                //                     echo $spell_name;
-                //                 echo '</button>';
-                //             echo '</form>';
-                //         }
-                //     }
-                // }
+                foreach($dice_data -> button as $key => $value) {
+                    if (isset($_GET[$value -> name])){
+
+                        // print_r($value -> parent_foreign_key);
+                        // print_r($value -> id);
+
+                        $parent_key =  $value -> id;
+                        $api_roll_content = "https://dnd-rolling-chart-api.herokuapp.com/api/button/sub/viewAll/children/${parent_key}";
+                        $json_api_roll_content = file_get_contents($api_roll_content);
+                        $roll_content_data = json_decode($json_api_roll_content);
+                        $random_roll_num = mt_rand(0, count($roll_content_data -> button) - 1);
+                        $random_roll_value = $roll_content_data -> button[$random_roll_num] -> value;
+                        $random_roll_display_num = $random_roll_num + 1;
+
+                        // print_r ($random_roll_value);
+
+                        echo "<div class='container-fluid'>";
+                            echo "<div class='row justify-content-center'>";
+                                echo "<div class='col-11 col-md-6 output spell-display py-5'>";
+                                    echo "<p class='spell-key mb-0'>You rolled a: ${random_roll_display_num}</p>";
+                                    echo "<p class='mb-0'><strong>${random_roll_value}</strong></p>";
+                                echo "</div>";
+                            echo "</div>";
+                        echo "</div>";
+                    }
+                }
             ?>
         </div>
     </h4>
-
-    <h5>
-
-    </h5>
-
-
 
 </body>
 </html>
